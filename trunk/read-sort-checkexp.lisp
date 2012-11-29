dcf8d4daf4115276fcefb122200ed6546c70a6b7
dstory23@gmail.com <dstory23@gmail.com@dcb35b65-c1f0-5a70-d8d1-d4f19cce79ad>
2012-11-29 05:20:44 +0000

    added check expects

--- trunk/read-sort-checkexp.lisp
+++ trunk/read-sort-checkexp.lisp
@@ -1,6 +1,6 @@
 (include-book "testing" :dir :teachpacks)
 (include-book "read-sort")
-
+(include-book "doublecheck" :dir :teachpacks)
 
 (check-expect (check-day 20121131) t)
 (check-expect (check-day 20120101) nil)
@@ -12,3 +12,57 @@
 (check-expect (fmt-date 20120931) 20121001)
 (check-expect (fmt-date 20120831) 20120901)
 (check-expect (fmt-date 19991231) 20000101)
+
+
+(check-expect (split-csv-style nil) nil)
+(check-expect (split-csv-style '((#\G #\O #\O #\G #\, 
+                              #\2 #\0 #\1 #\2 #\1 #\1 #\1 #\3 #\, 
+                              #\2 #\0 #\1 #\2 #\0 #\3 #\0 #\5)))
+              '(("GOOG" "20121113" "20120305")))
+(check-expect (split-csv-style '(
+      (#\G  #\O  #\O  #\G  #\,
+      #\2  #\0  #\1  #\2  #\0  #\1  #\0  #\5  #\,
+      #\2  #\0  #\1  #\2  #\0  #\3  #\0  #\5)
+      (#\A  #\M  #\Z  #\N  #\, 
+       #\2  #\0  #\1  #\2  #\0  #\1  #\0  #\5  #\,
+       #\2  #\0  #\1  #\2  #\0  #\3  #\0  #\5)
+      (#\Y  #\H  #\O  #\O  #\,
+       #\2  #\0  #\1  #\2  #\0  #\1  #\0  #\5  #\,
+       #\2  #\0  #\1  #\2  #\0  #\3  #\0  #\5)
+      (#\A  #\P  #\P  #\L  #\,
+       #\2  #\0  #\1  #\2  #\0  #\1  #\0  #\5  #\,
+       #\2  #\0  #\1  #\2  #\0  #\3  #\0  #\5)
+      (#\V  #\Z  #\W  #\,
+       #\2  #\0  #\1  #\2  #\0  #\1  #\0  #\5  #\,
+       #\2  #\0  #\1  #\2  #\0  #\3  #\0  #\5)
+      (#\G  #\O  #\O  #\G  #\,
+       #\2  #\0  #\1  #\2  #\0  #\1  #\0  #\5  #\,
+       #\2  #\0  #\1  #\2  #\0  #\3 #\0 #\5)))
+              '(("GOOG" "20120105" "20120305")
+ ("AMZN" "20120105" "20120305")
+ ("YHOO" "20120105" "20120305")
+ ("APPL" "20120105" "20120305")
+ ("VZW" "20120105" "20120305")
+ ("GOOG" "20120105" "20120305")))
+
+              
+(check-expect (to-search-structure nil) nil)
+
+
+
+(defrandom random-date ()
+  (random-between 19000101 20130101))
+
+(defrandom random-cp ()
+  (random-between 50 700))
+(defrandom random-tick ()
+  (random-string))
+
+(defrandom random-tree-input ()
+  (list (random-tick) (random-date) (random-tick)))
+
+(defrandom random-tree-input-list ()
+  (random-list-of (random-tree-input)))
+  
+(defrandom tree-builder ()
+  (get-info (random-tree-input-list)))
