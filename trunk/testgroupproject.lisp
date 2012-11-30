48e89de311b507f81b8d66bd39d5ea2797a97904
kobaj.g@gmail.com <kobaj.g@gmail.com@dcb35b65-c1f0-5a70-d8d1-d4f19cce79ad>
2012-11-30 17:45:56 +0000

    Assisting with testgroupproject code. It should now build a tree with nodes such that the node's key is the ticker symbol, and the node's datum is a subtree. This subtree contains nodes such that the subtree node's key is the ticker date, and the subtree node's datum is the closing price.

--- trunk/testgroupproject.lisp
+++ trunk/testgroupproject.lisp
@@ -2,20 +2,32 @@
 
 (include-book "list-utilities" :dir :teachpacks)
 (include-book "io-utilities" :dir :teachpacks)
-(include-book "avl-rational-keys" :dir :teachpacks)
+(include-book "avl-string-keys")
 (set-state-ok t)
 
+(defun insert-into-tree (xs tree)
+   (let* ((tk (car xs))
+             (td (cadr xs))
+             (cp  (caddr xs))
+             (old-tree (avl-retrieve tree tk))
+             (new-tree 
+        (if (equal old-tree nil)
+            (avl-insert tree tk (avl-insert (empty-tree) td cp))
+            (avl-insert tree tk (avl-insert (cdr old-tree) td cp)))))
+        new-tree))
 
 ; this fuction takes a list of lists where each list of strings where each
 ;list contains the ticker, the closing price and the trade date. Then it builds an AVL
 ;tree using the tk as the key, and the closing price and trade date as the data
-(defun build-tree (xs)
+(defun delegate-into-tree (xs tree)
+  (if (consp (cdr xs))
+      (delegate-into-tree (cdr xs) (insert-into-tree (car xs) tree))
+      (insert-into-tree (car xs) tree)))
+
+(defun parse-input (xs)
   (if (consp xs)
-      (let* ((tk (caar xs))
-             (cp (chrs->str (car(tokens '(#\< #\c #\p #\>)(str->chrs (cadr(car xs)))))))
+       (let* ((cp (chrs->str (car(tokens '(#\< #\c #\p #\>)(str->chrs (cadr(car xs)))))))
              (td (chrs->str (car(tokens '(#\< #\t #\d #\>)(str->chrs (caddr(car xs)))))))
-             (key (chrs->str (car(tokens '(#\< #\t #\k #\>)(str->chrs tk)))))
-             (data (list td cp))
-             (tr  (avl-insert (build-tree(cdr xs)) key data)))
-        tr)
-         (empty-tree)))
+             (key (chrs->str (car(tokens '(#\< #\t #\k #\>)(str->chrs (caar xs)))))))
+         (cons (list key td cp) (parse-input (cdr xs))))
+       nil))
