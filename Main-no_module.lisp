5da5d7da814a323a67a0b6b14ea14b17053fabe8
kobaj.g@gmail.com <kobaj.g@gmail.com@dcb35b65-c1f0-5a70-d8d1-d4f19cce79ad>
2012-11-30 17:45:56 +0000

    Assisting with testgroupproject code. It should now build a tree with nodes such that the node's key is the ticker symbol, and the node's datum is a subtree. This subtree contains nodes such that the subtree node's key is the ticker date, and the subtree node's datum is the closing price.

--- Main-no_module.lisp
+++ Main-no_module.lisp
@@ -2,8 +2,14 @@
 
 (in-package "ACL2")
 
+(include-book "minput") ;clay 2
 (include-book "testgroupproject") ;femi 2
-(include-book "read-sort") ;shane 3
-(include-book "minput") ;clay 4
-(include-book "htmlgenerator") ;cezar 5
+;(include-book "read-sort") ;shane 3
+;(include-book "htmlgenerator") ;cezar 5
 
+(set-state-ok t)
+
+
+(delegate-into-tree
+ (parse-input (file->tuples "../hist_short.txt" state))
+ (empty-tree))
