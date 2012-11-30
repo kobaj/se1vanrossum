5ee0dd3a0293a0179ca89b84e2dc1ce547be9ca8
kobaj.g@gmail.com <kobaj.g@gmail.com@dcb35b65-c1f0-5a70-d8d1-d4f19cce79ad>
2012-11-30 23:16:16 +0000

    date subtree now built more correctly.

--- trunk/Main-no_module.lisp
+++ trunk/Main-no_module.lisp
@@ -14,3 +14,14 @@
 (delegate-into-tree
  (parse-input (file->tuples "../hist_short.txt" state))
  (empty-tree))))
+
+(defun make-subtree()
+  (let* ((start-tree (empty-tree))
+         (start-tree2 (avl-insert start-tree "20120101" "2.0"))
+         (start-tree3 (avl-insert start-tree2 "20120202" "3.0"))
+         (start-tree4 (avl-insert start-tree3 "20130101" "4.0")))
+    start-tree4))
+
+(defun make-tree ()
+  (avl-insert (empty-tree) "GOOG"
+              (make-subtree)))
